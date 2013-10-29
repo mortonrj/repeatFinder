0484640d12ff4d38b1c9520d75fa1cce8fa48c36
Karro <karroje@eas-karroimac27.local>
2013-10-29 10:05:51 -0400

    Adding simulate_chr.py.

---
+++ src/simulate_chr.py
@@ -0,0 +1,16 @@
+from simulation import go
+import re
+# import biopython libraries
+
+
+def main(???):
+    # 1) Read in chr22.fa using biopython
+    # 2) Gnerate a simulated sequence s using go()
+    # 3) Read in chr22.fa.out  (using re.split("\s+", line))
+    # 4) Start replacing porsions of s with porsions of chr22.fa as dictated by lines of chr22.fa.out  
+    # (might combine (3) and (4) into one loop)
+    # 5) Write simulated sequence to new file using biopython
+
+
+if __name__ == "__main__":
+    main()
