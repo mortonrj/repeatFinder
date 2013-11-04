9898959276382872bbb03d5c3e6eedd69da9aa7f
git user <mortonrj@ubuntu.(none)>
2013-11-03 22:57:12 -0500

    Adding parser

--- src/simulate_chr.py
+++ src/simulate_chr.py
@@ -1,11 +1,18 @@
 from simulation import go
 import re
+import Bio
 # import biopython libraries
 
 
-def main(???):
+def main():
     # 1) Read in chr22.fa using biopython
-    # 2) Gnerate a simulated sequence s using go()
+
+    from Bio.seq import SeqIO
+    directory = "../genomes"
+    directory+"chr22.fa"
+    record = SeqIO.read(directory, "FASTA")
+    
+    # 2) Generate a simulated sequence s using go()
     # 3) Read in chr22.fa.out  (using re.split("\s+", line))
     # 4) Start replacing porsions of s with porsions of chr22.fa as dictated by lines of chr22.fa.out  
     # (might combine (3) and (4) into one loop)
