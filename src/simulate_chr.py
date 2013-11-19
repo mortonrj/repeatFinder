88a7dc9cabdb30d3a76dbd6f2bb883c3952db029
git user <mortonrj@ubuntu.(none)>
2013-11-19 00:14:41 -0500

    Adding repeats

--- src/simulate_chr.py
+++ src/simulate_chr.py
@@ -9,23 +9,34 @@ def main():
 
     from Bio.seq import SeqIO
     directory = "../genomes"
-    directory+"chr22.fa"
-    record = SeqIO.read(directory, "FASTA")
+    genomeFile=directory+"/chr22.fa"
+    record = SeqIO.read(genomeFile, "FASTA")
     
     # 2) Generate a simulated sequence s using go()
     k = 5
     length = len( str(record))
-    go(record, k, length)
+    practice = list()
+    genome = list(record.seq)
+    # go(record, k, length)
+    for l in length:
+        practice.append(random.sample(['A','G','C','T'], 1))
+
     
     # 3) Read in chr22.fa.out  (using re.split("\s+", line))
-    repeatRec = SeqIO.read(director, "FASTA")
-    repeatRec("\s+", line)
+    repeatFile =directory+"/chr22.fa.out"
+    with open(repeatFile,'w') as repeatRec:
+        for line in repeatRec:
+            line = line.rstrip()
+            toks = re.split("\s+", line)
+            start,end = int(toks[5]), int(toks[6])
+            practice[start:end] = genome[start:end]
+    practice = Seq("".join(practice))
+            
+    # 4) Start replacing portions of s with portions of chr22.fa as dictated by lines of chr22.fa.out  
     
-    # 4) Start replacing porsions of s with porsions of chr22.fa as dictated by lines of chr22.fa.out  
-    newString = record
     # (might combine (3) and (4) into one loop)
     # 5) Write simulated sequence to new file using biopython
-    output_handle = open ("example.fasta", "w")
+    output_handle = open ("exampleFasta.fasta", "w")
     SeqIO.write(newString, output_handle, "fasta")
 
 if __name__ == "__main__":
