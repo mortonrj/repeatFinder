ade73c76f929293ea2b5b3977e7ad5c46c641d4e
mortonrj <morton.rjt@gmail.com>
2013-12-27 15:16:00 -0500

    Added negative strand option

--- src/simulate_chr.py
+++ src/simulate_chr.py
@@ -9,7 +9,7 @@ import random
 
 
 
-def main(seq,k,r,name):
+def main(seq,k,r,name,negative):
     # 1) Read in sequence using biopython
     directory = "../genomes"
     genomeFile = directory+"/"+seq
@@ -26,12 +26,6 @@ def main(seq,k,r,name):
     repeatFile =directory+"/test.fa.out"
     repeatRec = open(repeatFile,'r')
 
-    # Moving past first few lines of code
-    # repeatRec.readline()
-    # repeatRec.readline()
-    # repeatRec.readline() 
-    
-    
     lines = repeatRec.readlines()
     for i in range(3,len(lines)):
         line = lines[i]
@@ -40,37 +34,45 @@ def main(seq,k,r,name):
         line = line.rstrip()
         toks = re.split("\s+", line)
 
-        #Replacing portions of s with portions of chr22.fa as dictated by lines of chr22.fa.out
-        start,end = int(toks[6]), int(toks[7])
-        newSeq[start:end] = list(record)[start:end]
-    newSeq = Seq("".join(newSeq))
-    print "Repeat: " + Seq("".join(list(record)[start:end]))
-    print "New sequence: " + newSeq
-    
- 
-    #Write simulated sequence to new file using biopython  
+        # Checking if including repeats on negative strand
+
+        if negative is False and toks[9] == 'C':
+            System.out.println("negative not included")
+        else:
+            #Replacing portions of s with portions of chr22.fa as dictated by lines of chr22.fa.out
+            start,end = int(toks[6]), int(toks[7])
+            newSeq[start:end] = list(record)[start:end]
+
+        newSeq = Seq("".join(newSeq))
+        print "New sequence: " + newSeq
+
+
+    #Write simulated sequence to new file using biopython
     output_handle = open(name, "w")
     simSeq = SeqRecord(newSeq)
     SeqIO.write(simSeq, output_handle, "fasta")
 
 if __name__ == "__main__":
     # main()
-   
+
     parser = argparse.ArgumentParser(description="Generate Simulated Sequence")
     parser.add_argument(\
         '--seq',action='store',required=False,type = str,default = "test.fa",
-        help="sequence fasta file")     
+        help="sequence fasta file")
     parser.add_argument(\
         '-k',action='store',dest='k',type=int,default=5,
-        help="order of Markov chain, default is 5")     
+        help="order of Markov chain, default is 5")
     # Will be more applicable later, at the moment, the test repeat file only has two repeats
     parser.add_argument(\
         '-r','--repeat',action='store',dest='repeat',type=str,
-        help="The repeat that you are choosing to include in the simulated sequence") 
+        help="The repeat that you are choosing to include in the simulated sequence")
     parser.add_argument(\
         '-n','--name',action='store',dest='name',default = "result.fa",
         help="name of simulated sequence")
+    parser.add_argument(\
+        '-c','--negative',action='store',dest='negative',type = bool, default = False,
+        help = "boolean value for if the repeats on the negative strandare included")
 
     args = parser.parse_args()
 
-    main(args.seq,args.k,args.repeat,args.name)
+    main(args.seq,args.k,args.repeat,args.name,args.negative)
